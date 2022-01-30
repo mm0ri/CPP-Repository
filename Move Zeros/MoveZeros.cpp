7f91fc059dc9680018c24119ee63791d95419242
Vadim <Shkuratvadim@gmail.com>
2022-01-31 01:14:14 +0300

    Move Zeros CPP project was uploaded
    Project from codewars on cpp.

---
+++ Move Zeros/MoveZeros.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<int> z_array = {1, 2, 0, 1, 0, 1, 0, 3, 0, 1};
+
+    unsigned int vector_size = z_array.size();
+
+    for (int i = 0; i < vector_size; i++)
+    {
+        if (z_array[i] == 0)
+        {
+            z_array.push_back(z_array[i]);
+            z_array.erase(z_array.begin() + i);
+        } 
+        std::cout << z_array[i] << ",";
+    }
+    return z_array;
+}
